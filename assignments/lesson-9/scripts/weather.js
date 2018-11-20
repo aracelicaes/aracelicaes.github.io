ca02fa2b1f5a126a2f7b5c4ecacfdaf593d4555f
Camarillo-Araceli <aracelicaes@users.noreply.github.com>
2018-11-20 00:31:39 -0600

    c

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -32,52 +32,19 @@ request.onload = function () {
 */
 
 }
-/*
-var forecast = new XMLHttpRequest();
-forecast.open('GET', "", true);
-forecast.send();
-forecast.onload = function () {
-  var prestonForecast = JSON.parse(forecast.responseText);
-  console.log(prestonForecastt);
-  document.getElementById("daily").innerHTML = prestonForecast.forecast.txt_forecast.forecastday["0"].fcttext;
 
+//FORECAST HERE
+var requestURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5061036&APPID=28503afc8c3e5c6c63a0c4607d300be4&cnt=7";
+var request2 = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
+request.onload = function () {
 
+  //preston
+  var prestonWeatherResp2 = request.response;
+  var prestonWeather2 = JSON.parse(prestonWeatherResp);
+  console.log(prestonWeather);
+  console.log(prestonWeatherResp);
+  document.getElementById("pweather").innerHTML = prestonWeather.weather[0].main;
 }
-//Soda Springs
-var soda = new XMLHttpRequest();
-soda.open('GET', "", true);
-soda.send();
-soda.onload = function () {
-  var sodaWeather = JSON.parse(soda.responseText);
-  document.getElementById("currentg").innerHTML = sodaWeather.current_observation.weather;
-  document.getElementById("temperatureg").innerHTML = sodaWeather.current_observation.temp;
-  document.getElementById("windspeedg").innerHTML = sodaWeather.current_observation.speed;
-  document.getElementById("windspeedg").innerHTML = sodaWeather.current_observation.speed;
-  //document.getElementById("windchillg").innerHTML = sodaWeather.current_observation.windchill_f;
-  document.getElementById("dailyg").innerHTML = sodaWeather.forecast.txt_forecast.forecastday["0"].fcttext;
-  document.getElementById("currenticong").innerHTML = "<img src= \"" + sodaWeather.current_observation.icon_url + "\" > ";
-
-  var str = document.getElementById("currenticong").innerHTML;
-  var rep = str.replace("http:", "https:");
-  document.getElementById("currenticong").innerHTML = rep;
-
-
-}
-//Fish Haven
-var fishHaven = new XMLHttpRequest();
-fishHaven.open('GET', "", true);
-fishHaven.send();
-fishHaven.onload = function () {
-  var fishHavenWeather = JSON.parse(fishHaven.responseText);
-  document.getElementById("currents").innerHTML = fishHavenWeather.current_observation.weather;
-  document.getElementById("temperatures").innerHTML = fishHavenWeather.current_observation.temp_f;
-  document.getElementById("windspeeds").innerHTML = fishHavenWeather.current_observation.wind_mph;
-  //document.getElementById("windchills").innerHTML = fishHavenWeather.current_observation.windchill_f;
-  document.getElementById("dailys").innerHTML = fishHavenWeather.forecast.txt_forecast.forecastday["0"].fcttext;
-  document.getElementById("currenticons").innerHTML = "<img src= \"" + fishHavenWeather.current_observation.icon_url + "\" > ";
-
-  var str = document.getElementById("currenticons").innerHTML;
-  var rep = str.replace("http:", "https:");
-  document.getElementById("currenticons").innerHTML = rep;
-
-}*/
