ff79e2f31f03c8d5b40ca900da6bf35d2ec6bc13
Camarillo-Araceli <aracelicaes@users.noreply.github.com>
2018-11-20 00:00:02 -0600

    c

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -1,7 +1,7 @@
 var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5061036&APPID=28503afc8c3e5c6c63a0c4607d300be4";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-request.responseType = 'json';
+request.responseType = 'text';
 request.send();
 request.onload = function () {
 
